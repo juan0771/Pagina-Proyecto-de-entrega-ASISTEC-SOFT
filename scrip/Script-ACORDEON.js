a8fe3a807a85cca95d22e32b7830dfdfc8d6f04d
Juan Camilo Andrade <61404137+juan0771@users.noreply.github.com>
2025-03-13 00:07:40 -0500

    Pagina Proyecto de entrega ASISTEC-SOFT V1
    Pagina Proyecto de entrega ASISTEC-SOFT V1

---
+++ scrip/Script-ACORDEON.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+
+$(".accordion-titulo").click(function () {
+
+    var contenido = $(this).next(".accordion-content");
+
+    if (contenido.css("display") == "none") { //open		
+        contenido.slideDown(250);
+        $(this).addClass("open");
+    }
+    else { //close		
+        contenido.slideUp(250);
+        $(this).removeClass("open");
+    }
+});
+});
